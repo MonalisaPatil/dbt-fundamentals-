c32428d7d802303a1ec6a282e473d91ad30099e0
Monalisa Patil <mpatil5@buffalo.edu>
2022-05-03 00:17:20 +0000

    dbt update

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,12 +1,11 @@
-with orders as (
-    
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-    from raw.jaffle_shop.orders
-)
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
-select * from orders
+from raw.stripe.payment 
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,12 +1,11 @@
-with orders as (
-    select
-        id as payment_id,
-         orderid as order_id,
-        paymentmethod as payment_method,
-        status,
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-        -- amount is stored in cents, convert it to dollars
-        amount / 100 as amount,
-        created as created_at
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
-from {{source('stripe','payments')}}
+from raw.stripe.payment 
